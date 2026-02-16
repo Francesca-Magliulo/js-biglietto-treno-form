1347201bce20e62db9e558b00663d631edc8e950
Francesca-Magliulo <github.mf@gmail.com>
2026-02-16 19:15:59 +0100

    pulizia commenti milestone2

--- milestone2/js/script.js
+++ milestone2/js/script.js
@@ -36,70 +36,7 @@ function prezzo(evento) {
 }
 
 
-/*
-let chilometri = document.getElementById("km-field")
-let eta = document.getElementById("eta-field")
 
-let form = document.querySelector("form")
-
-form.addEventListener("submit", prezzo)
-
- function calcolaPrezzo (chilometri, eta) {
-
-    let prezzoBase = chilometri * 0.21;
-    let prezzoFinale;
-
-    if (eta < 18) {
-        prezzoFinale = prezzoBase - (prezzoBase * 20 / 100);
-    } else if (eta> 65) {
-        prezzoFinale = prezzoBase - (prezzoBase * 40 / 100);
-    } else {
-        prezzoFinale = prezzoBase;
-    }
-    return prezzoFinale
- }
-
-function prezzo(evento) {
-
-    evento.preventDefault()
-
-    let numeroKm = parseFloat(chilometri.value)
-    let numeroEta = parseInt(eta.value)
-    
-
-    if(!validaInput(numeroKm, numeroEta)) {
-        return
-    }
-    
-    let prezzoFinale = calcolaPrezzo(numeroKm, numeroEta)
-
-    let kmCard = document.getElementById("km-card")
-    let etaCard = document.getElementById("eta-card")
-    let priceCard = document.getElementById("price-card")
-
-
-
-    kmCard.innerHTML = numeroKm
-    etaCard.innerHTML = numeroEta
-    priceCard.innerHTML = prezzoFinale.toFixed(2)
-
-    eta.value = ""
-    chilometri.value = ""
-
-}
-
-
-function validaInput(numeroKm, numeroEta) {
-
-    if(isNaN(numeroKm) || numeroKm <= 0 ) {
-     return false   
-    }else if (isNaN(numeroEta) || numeroEta <= 0) {
-        return false
-    } else {
-        return true
-        }
-}
-*/
 
 
 
